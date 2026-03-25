1e6475155121c19f491c908ae22d8c7501069e5a
Hà Thúc Khương Ninh <hathuckhuongninh@gmail.com>
2026-03-25 20:59:21 +0700

    Add files via upload

---
+++ Clean_data.sql
@@ -0,0 +1,12 @@
+﻿-- Bước 1: Cập nhật 1/0 thành Yes/No
+UPDATE [dbo].[employee_attrition]
+SET Attrition = CASE 
+                   WHEN Attrition = 1 THEN 'Yes'
+                   WHEN Attrition = 0 THEN 'No'
+                END
+-- Bước 2: Xóa các dòng có giá trị NULL
+DELETE FROM [dbo].[employee_attrition]
+WHERE Attrition IS NULL;
+
+select *
+from [dbo].[employee_attrition]
